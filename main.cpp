0d05d10551d9a63297a3db9f633a3a2a3eec022a
TTF-fog <adh.ishaan10@gmail.com>
2025-02-25 11:32:58 +0530

    updates, fix opt_params

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <map>
 #include <utility>
 #include <vector>
 #include <string>
@@ -9,30 +10,51 @@ class TrayItem {
         std::string type;
         std::string name;
         std::string callback;
-        toml::table opt_params;
+        std::map<std::string,std::string> opt_params;
         Tray::Tray &tray;
 
-        TrayItem(std::string type, std::string name,toml::table opt_params, Tray::Tray &tray): tray(tray) {
-            this->name = std::move(name);
-            this->callback = std::move(callback);
-            this->opt_params = std::move(opt_params);
-            this->type = std::move(type);
-            this->tray = tray;
+        TrayItem(std::string type, std::string name, std::map<std::string,std::string> opt_params,Tray::Tray &tray): tray(tray), name(name), opt_params(opt_params), type(type) {
             populate();
         }
 
     private:
         void populate() {
             if (this->type == "button") {
-                tray.addEntry(Tray::Button(this->name, [&]{
-                    exec(opt_params["callback"]);
+                try {
+                    std::string v = this->opt_params.at("callback");
+                    tray.addEntry(Tray::Button(this->name, [v, this]{
+                        exec( v.c_str() );
 
                   }));
+                }
+                catch (std::exception e) {
+                    std::cerr << "Could not find callback skipping " << this->name << std::endl;
+                }
+
 
             }else{
                 std::cerr << "Error: " << type << "is not a type"<< std::endl;
             }
         }
+    std::string exec(const char* cmd) {
+            std::array<char, 128> buffer;
+            std::string result;
+            std::unique_ptr<FILE, void(*)(FILE*)> pipe(popen(cmd, "r"),
+            [](FILE * f) -> void
+            {
+                // wrapper to ignore the return value from pclose() is needed with newer versions of gnu g++
+                std::ignore = pclose(f);
+            });
+            if (!pipe) {
+                throw std::runtime_error("popen() failed!");
+            }
+            while (fgets(buffer.data(), static_cast<int>(buffer.size()), pipe.get()) != nullptr) {
+                result += buffer.data();
+            }
+
+            return result;
+        }
+
 
 };
 class trayApp {
@@ -41,14 +63,16 @@ public:
     std::vector<TrayItem> items;
     trayApp(const std::string &name, const std::string &icon){
         Tray::Tray tray (name,icon);
+
         auto data = toml::parse("config.toml");
         this->entries = toml::find<std::vector<std::string>>(data, "main", "entry_points");
         for (const auto& entry : this->entries) {
             this->items.push_back(TrayItem(
                 toml::find<std::string>(data, entry, "type"),
                 toml::find<std::string>(data, entry, "name"),
-                toml::find<toml::table>(data, entry, "opt_params"),
+                toml::find<std::map<std::string, std::string>>(data, entry, "opt_params"),
                 tray
+
                 ));
         }
         tray.run();
